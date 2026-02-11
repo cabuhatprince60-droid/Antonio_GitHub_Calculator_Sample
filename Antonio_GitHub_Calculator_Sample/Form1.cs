60ab92f51fa65a5cdc9731c779ad4fd4b7b8a36d
marischkaantonio <marischkaantonio7@gmail.com>
2026-02-11 19:27:15 +0800

    feat:layoutdone

--- Antonio_GitHub_Calculator_Sample/Form1.cs
+++ Antonio_GitHub_Calculator_Sample/Form1.cs
@@ -16,5 +16,15 @@ namespace Antonio_GitHub_Calculator_Sample
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
